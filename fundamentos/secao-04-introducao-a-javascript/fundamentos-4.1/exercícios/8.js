bfb223e8ec1420556a2d2f78b14bb35b13373c48
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-11-26 09:45:42 -0300

    exercicio 8 da lista de exercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.1/exercícios/8.js
@@ -0,0 +1,8 @@
+//Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+//Bonus: use somente um if.
+const numberA = 1;
+const numberB = 5;
+const numberC = 3;
+if (numberA%2===0 || numberB%2===0 || numberC%2===0){
+    console.log('true');
+} else {console.log('false')}
