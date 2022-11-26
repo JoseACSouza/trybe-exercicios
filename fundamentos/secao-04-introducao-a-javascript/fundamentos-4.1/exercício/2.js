7182b10c5ba49cb6313139afb259108de777eb2a
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-11-26 08:13:26 -0300

    exercicio 2 da lista de exercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.1/exercício/2.js
@@ -0,0 +1,8 @@
+// Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
+let numberA = 4;
+let numberB = 4;
+if (numberA > numberB){
+    console.log('O número',numberA, 'é maior que o número',numberB);
+} else if (numberB > numberA){
+    console.log('O número', numberB,'é maior que', numberA);
+} else {console.log('Os números são iguais')};
