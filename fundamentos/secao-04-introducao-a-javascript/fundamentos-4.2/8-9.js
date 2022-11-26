745ad44f32cb8ab9878043c7a91bd2e785f9390e
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-11-26 14:36:03 -0300

    atividade 9

--- fundamentos/secao-04-introducao-a-javascript/fundamentos-4.2/8-9.js
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.2/8-9.js
@@ -1,6 +1,11 @@
 //Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 let numbers = [];
+let result = [];
 for (let index = 1; index < 26; index+=1){
     numbers.push(index);
 }
+for (let index =0; index < numbers.length; index+=1){
+    result.push(numbers[index]/2);
+}
 console.log(numbers);
+console.log(result);
