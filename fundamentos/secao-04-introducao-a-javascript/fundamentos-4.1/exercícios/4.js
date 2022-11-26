4d1f28470adb162533070f8e1f56122679d2374d
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-11-26 08:54:17 -0300

    exercicio 4 da lista de exercicios

--- fundamentos/secao-04-introducao-a-javascript/fundamentos-4.1/exercícios/4.js
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.1/exercícios/4.js
@@ -1,7 +1,7 @@
 // Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
-let number = 4;
-if (number > numberB){
-    console.log('O número',number, 'é maior que o número',number);
-} else if (numberB > numberA){
-    console.log('O número', numberB,'é maior que', numberA);
-} else {console.log('Os números são iguais')};
+let number = 0;
+if (number > 0){
+    console.log('positive');
+} else if (0 > number){
+    console.log('negative');
+} else {console.log('zero')};
