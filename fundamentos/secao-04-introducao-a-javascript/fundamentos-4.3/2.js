684f9fc115d03fe7898e10999626ffa2c24b6a0e
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-11-29 18:28:02 -0300

    atividade 1, inversor de palavra      - edit - salvando a palavra em uma nova string e não só mostrando na tela como tava antes

--- fundamentos/secao-04-introducao-a-javascript/fundamentos-4.3/2.js
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.3/2.js
@@ -1,7 +1,9 @@
 //Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
 let word = 'banana';
+let reverseWord = ''
 for (let index = word.length-1; index>=0; index-=1 ){
-    console.log(word[index]);
+    reverseWord += word[index];
 }
+console.log(reverseWord);
 // -1 no length por que a contagem de caracteres começam em 1, enquanto a contagem de posição em 0
