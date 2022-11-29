bccf3dac6824c271dbfddef7a8da5f5a1e42e6cf
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-11-29 19:00:50 -0300

    atividade 3, maior palavra e menor palavra

--- fundamentos/secao-04-introducao-a-javascript/fundamentos-4.3/3.js
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.3/3.js
@@ -3,12 +3,23 @@
 
 // MAIOR PALAVRA
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maxWidth = 0;
-let position = 0;
+let maxWidth = Number.NEGATIVE_INFINITY;
+let maxPosition = 0;
 for(let index = 0; index < array.length; index+=1){
     if (array[index].length > maxWidth){
         maxWidth = array[index].length;
-        position = index;
+        maxPosition = index;
     }
 } 
-console.log(array[position]);
+console.log('Maior palavra: ' + array[maxPosition]);
+
+// MENOR PALAVRA
+let minWidth = Number.POSITIVE_INFINITY;
+let minPosition = 0;
+for(let index = 0; index < array.length; index+=1){
+    if (array[index].length < minWidth){
+        minWidth = array[index].length;
+        minPosition = index;
+    }
+} 
+console.log('Menor palavra: ' + array[minPosition]);
