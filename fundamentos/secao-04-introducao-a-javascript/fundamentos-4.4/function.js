b958913c0efd69ca63d4431d2325989687455b7e
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-12-02 17:29:29 -0300

    resolução do item 6 da atividade sobre funções

--- fundamentos/secao-04-introducao-a-javascript/fundamentos-4.4/function.js
+++ fundamentos/secao-04-introducao-a-javascript/fundamentos-4.4/function.js
@@ -83,4 +83,18 @@ const repeatNumbers = (repeat) =>{
 return repeat[highestValue(numberOfRepeats)];
 //Highestvalue é a função pedida no item 2 desta lista 
 }
-console.log(repeatNumbers([2, 3, 2, 5, 8, 2, 3]));
+//console.log(repeatNumbers([2, 3, 2, 5, 8, 2, 3]));
+
+////////////////////////////////////-> ITEM 6 <-///////////////////////////////////
+// Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+// Valor de teste: N = 5.
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+
+const summation = (numberN) => {
+    let numberAssistant = 0;
+    for (let index = numberN; index > 0; index-=1){
+        numberAssistant +=index;          
+    }
+return numberAssistant;
+}
+//console.log(summation(5));
