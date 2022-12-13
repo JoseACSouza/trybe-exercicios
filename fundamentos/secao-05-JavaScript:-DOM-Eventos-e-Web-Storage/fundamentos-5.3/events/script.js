9584cc887c2098341a14d65a8310e1957459fdf2
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-12-13 12:01:32 -0300

    comitando

--- fundamentos/secao-05-JavaScript:-DOM-Eventos-e-Web-Storage/fundamentos-5.3/events/script.js
+++ fundamentos/secao-05-JavaScript:-DOM-Eventos-e-Web-Storage/fundamentos-5.3/events/script.js
@@ -13,5 +13,26 @@ const createDaysOfTheWeek = () => {
   
   createDaysOfTheWeek();
   
+
   // Escreva seu código abaixo.
-  
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+    const createDays = () =>{
+    const addIdDays = document.querySelector('#days');
+
+    for (let index = 0; index < decemberDaysList.length; index +=1){
+    
+    const createLi = document.createElement('li');
+    createLi.className = 'day';
+    createLi.innerHTML = decemberDaysList[index];
+    addIdDays.appendChild('createLi');
+
+    if( decemberDaysList[index] === 24 || decemberDaysList[index] === 31 ){
+      createLi.classList.add('holiday');
+    } else if (decemberDaysList[index] === 4|| decemberDaysList[index] === 11 || decemberDaysList[index] === 18 ){
+      createLi.classList.add('friday');
+    } else if (decemberDaysList[index] === 25){
+    createLi.classList.add('friday', 'holiday');
+    }
+  }
+}
