da6119975d20c4e547a7cb02f7541d4c5769d261
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2022-12-13 13:16:08 -0300

    adiciona até a parte 2

--- fundamentos/secao-05-JavaScript:-DOM-Eventos-e-Web-Storage/fundamentos-5.3/events/script.js
+++ fundamentos/secao-05-JavaScript:-DOM-Eventos-e-Web-Storage/fundamentos-5.3/events/script.js
@@ -15,24 +15,42 @@ const createDaysOfTheWeek = () => {
   
 
   // Escreva seu código abaixo.
+
+// Parte 1
   const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
     const createDays = () =>{
-    const addIdDays = document.querySelector('#days');
+    const addIdDays = document.getElementById('days');
 
     for (let index = 0; index < decemberDaysList.length; index +=1){
     
-    const createLi = document.createElement('li');
+    let day = decemberDaysList[index];
+    let createLi = document.createElement('li');
     createLi.className = 'day';
-    createLi.innerHTML = decemberDaysList[index];
-    addIdDays.appendChild('createLi');
 
-    if( decemberDaysList[index] === 24 || decemberDaysList[index] === 31 ){
+    if( day === 24 || day === 31 ){
       createLi.classList.add('holiday');
-    } else if (decemberDaysList[index] === 4|| decemberDaysList[index] === 11 || decemberDaysList[index] === 18 ){
+    } else if (day === 4|| day === 11 || day === 18 ){
       createLi.classList.add('friday');
-    } else if (decemberDaysList[index] === 25){
+    } else if (day === 25){
     createLi.classList.add('friday', 'holiday');
-    }
+   }
+   createLi.innerHTML = day;
+   addIdDays.appendChild(createLi);
   }
 }
+createDays();
+
+//Parte 2
+  const createButton = (feriados) => {
+    feriados = 'Feriados';
+    const button = document.getElementsByClassName('buttons-container')[0];
+    let newButton = document.createElement('button');
+    newButton.id = 'btn-holiday';
+    newButton.innerHTML = feriados;
+    button.appendChild(newButton);
+  }
+
+  createButton('feriados');
+
+//Parte 3
